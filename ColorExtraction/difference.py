7b62fb4a99cc61abf8bf9f7d4837d13d9cc931d0
hsuominen <hsuominen@gmail.com>
2014-09-27 17:01:09 +0200

    Deleting all at Giulio's request

--- ColorExtraction/difference.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageChops, ImageDraw
-
-im1 = Image.open('../imgs/Output/copenhagen-painting.jpg').convert('LA') #Convert to grayscale
-im2 = Image.open('../imgs/Output/copenhagen-infrared-ex.jpg').convert('LA')
-
-out = ImageChops.difference(im1,im2) 
-
-out.show()
