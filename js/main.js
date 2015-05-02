aa02dc84d6c461e82bf600161fae6e9078403237
Giulio Ungaretti <giulioungaretti@me.com>
2015-05-02 12:46:21 +0200

    Added jqery scrpit to create tabs.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+        //$('#tabs .tab-link').css('cursor','pointer');
+        $('#tabs .tab-link').click(function() {
+                console.log(this);
+                var tab_id = $(this).attr('data-tab');
+                console.log(tab_id);
+                $('#tabs .tab-link').removeClass('current');
+                $('.tab-content').removeClass('current');
+
+                $(this).addClass('current');
+                $("#" + tab_id).addClass('current');
+        });
+});
